f9a2f0754770deaf006fb934b2de7cfa71ff8e41
tobutoptours-kawai <kenji_kawai@tobutoptours.co.jp>
2021-04-20 15:11:09 +0900

    Delete Directory

--- tourlist/js/analytics.js
+++
@@ -1,8 +0,0 @@
-//Global site tag (gtag.js) - Google Analytics
-async src="https://www.googletagmanager.com/gtag/js?id=UA-144192745-1"
-
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-
-gtag('config', 'UA-144192745-1');
